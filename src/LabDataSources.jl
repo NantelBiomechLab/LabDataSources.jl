4d1b6090abaacdae0e145e1b783bf49088ea8e89
Allen Hill <allenofthehills@gmail.com>
2021-08-09 17:24:28 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.17

---
+++ src/LabDataSources.jl
@@ -0,0 +1,5 @@
+module LabDataSources
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using LabDataSources
+using Test
+
+@testset "LabDataSources.jl" begin
+    # Write your tests here.
+end
