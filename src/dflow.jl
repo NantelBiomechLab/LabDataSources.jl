1266492d0844750bc3263570ec859677df1a26e3
Allen Hill <allenofthehills@gmail.com>
2022-06-23 10:59:25 -0400

    Fix readsegment for DFlow sources

--- src/dflow.jl
+++ src/dflow.jl
@@ -1,13 +1,10 @@
-export DFlow, RawDFlowPDSource
+export DFlow, RawDFlowPD
 
 struct DFlow; end
 struct RawDFlow; end
 struct RawDFlowPD; end
 
-const DFlowSource = Source{DFlow}
-const RawDFlowPDSource = Source{RawDFlowPD}
-
-function DatasetManager.readsource(s::DFlowSource; kwargs...)
+function DatasetManager.readsource(s::Source{DFlow}; kwargs...)
     CSV.read(sourcepath(s), DataFrame; header=2, kwargs...)
 end
 
@@ -21,32 +18,24 @@ end
 
 function DatasetManager.readsegment(
     seg::Segment{O}; kwargs...
-) where O <: Union{Source{DFlow},Source{RawDFlow}}
-    timecol = (O isa DFlowSource) ? 1 : 2
-    columns, colnames = readsource(seg.source; kwargs...)
-    firsttime = first(columns[timecol])
-    lasttime  = last(columns[timecol])
+) where O <: Union{Source{DFlow},Source{RawDFlow},Source{RawDFlowPD}}
+    timecol = (O isa Source{DFlow}) ? 1 : 2
+    df = readsource(seg.source; kwargs...)
+    firsttime = first(df[!,timecol])
+    lasttime  = last(df[!,timecol])
+
+    firsttime ≤ something(seg.start, firsttime) ≤ lasttime ||
+        throw(error("$seg start time $(seg.start) is not within the source time range of $firsttime:$lasttime"))
+    firsttime ≤ something(seg.finish, lasttime) ≤ lasttime ||
+        throw(error("$seg finish time $(seg.finish) is not within the source time range of $firsttime:$lasttime"))
+
+    startidx = searchsortedfirst(df[!, timecol], something(seg.start, firsttime))
 
     if isnothing(seg.finish)
-        _finish = lasttime
+        finidx = lastindex(df[!, timecol])
     else
-        _finish = seg.finish
+        finidx = searchsortedlast(df[!, timecol], seg.finish)
     end
 
-    firsttime ≤ seg.start ≤ lasttime || throw(error("$s start time $(seg.start) is not within"*
-        "the source time range of $firsttime:$lasttime"))
-    firsttime ≤ _finish ≤ lasttime || throw(error("$s finish time $(seg.finish) is not within "*
-        "the source time range of $firsttime:$lasttime"))
-
-    startidx = searchsortedfirst(columns[timecol], firsttime)
-
-    if isnothing(seg.finish)
-        finidx = lastindex(columns[timecol])
-    else
-        finidx = searchsortedlast(columns[timecol], seg.finish)
-    end
-
-    segcolumns = ntuple(i -> columns[i][startidx:finidx], length(columns))
-
-    return segcolumns, colnames
+    return df[startidx:finidx,:]
 end
